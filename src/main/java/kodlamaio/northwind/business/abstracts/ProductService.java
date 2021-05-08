18a894f8a51ab797f7e34e2c3f3798ff1142c52d
Furkan Paşaoğlu <furkan.pasaoglu99@gmail.com>
2021-05-09 00:30:56 +0300

    Initial commit

---
+++ src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -0,0 +1,13 @@
+package kodlamaio.northwind.business.abstracts;
+
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
+import kodlamaio.northwind.entities.concretes.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> GetAll();
+    DataResult<List<Product>> getAll();
+    Result add(Product product);
+}
