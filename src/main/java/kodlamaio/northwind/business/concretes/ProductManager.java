822f52a9e79f7d70423871dcd99627c191e261b4
Furkan Paşaoğlu <furkan.pasaoglu99@gmail.com>
2021-05-23 00:29:28 +0300

    Updated

--- src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -28,12 +28,12 @@ public class ProductManager implements ProductService {
 
     @Override
     public DataResult<List<Product>> getAll() {
-        return new SuccessDataResult<List<Product>>(this.productDao.findAll(),"Data listelendi");
+        return new SuccessDataResult<List<Product>>(this.productDao.findAll(),"Data listelendi.");
     }
 
     @Override
     public Result add(Product product) {
         this.productDao.save(product);
-        return new SuccessResult("Ürün eklendi");
+        return new SuccessResult("Ürün eklendi.");
     }
 }
