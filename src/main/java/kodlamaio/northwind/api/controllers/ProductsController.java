f5dd57b59536d5a56a43d05f118ed4cc2a806ec5
Furkan Paşaoğlu <furkan.pasaoglu99@gmail.com>
2021-05-28 00:14:58 +0300

    Updated

--- src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -1,6 +1,7 @@
 package kodlamaio.northwind.api.controllers;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,9 +19,19 @@ public class ProductsController {
         this.productService = productService;
     }
 
-    @GetMapping("/getall")
-    public List<Product> getAll(){
-        return productService.GetAll();
+    @GetMapping("/getAll")
+    public DataResult<List<Product>> getAll(){
+        return productService.getAll();
+    }
+
+    @GetMapping("/getAllByPage")
+    public DataResult<List<Product>> getAll(int pageNo,int pageSize){
+        return this.productService.getAll(pageNo,pageSize);
+    }
+
+    @GetMapping("/getAllDesc")
+    public DataResult<List<Product>> getAllSorted(){
+        return this.productService.getAllSorted();
     }
 
     @PostMapping("/add")
@@ -28,4 +39,38 @@ public class ProductsController {
         return this.productService.add(product);
     }
 
+    @GetMapping("/getByProductName")
+    public DataResult<Product> getByProductName(@RequestParam("productName") String productName){
+        return this.productService.getByProductName(productName);
+    }
+
+    @GetMapping("/getByProductNameAndCategory_CategoryId")
+    public DataResult<Product> getByProductNameAndCategory_CategoryId(@RequestParam("productName") String productName,@RequestParam("categoryId") int categoryId){
+        return this.productService.getByProductNameAndCategory_CategoryId(productName,categoryId);
+    }
+
+    @GetMapping("/getByProductNameOrCategory_CategoryId")
+    public DataResult<List<Product>> getByProductNameOrCategory_CategoryId(@RequestParam("productName") String productName,@RequestParam("categoryId") int categoryId){
+        return this.productService.getByProductNameOrCategory_CategoryId(productName,categoryId);
+    }
+
+    @GetMapping("/getByCategoryIdIn")
+    public DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories){
+        return this.productService.getByCategoryIdIn(categories);
+    }
+
+    @GetMapping("/getByProductNameContains")
+    public DataResult<List<Product>> getByProductNameContains(@RequestParam("productName") String productName){
+        return this.productService.getByProductNameContains(productName);
+    }
+
+    @GetMapping("/getByProductNameStartsWith")
+    public DataResult<List<Product>> getByProductNameStartsWith(@RequestParam("productName") String productName){
+        return this.productService.getByProductNameStartsWith(productName);
+    }
+
+    @GetMapping("/getByNameAndCategory_CategoryId")
+    public DataResult<List<Product>> getByNameAndCategory_CategoryId(@RequestParam("productName") String productName,@RequestParam("categoryId") int categoryId){
+        return this.productService.getByNameAndCategory_CategoryId(productName,categoryId);
+    }
 }
--- src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -7,7 +7,15 @@ import kodlamaio.northwind.entities.concretes.Product;
 import java.util.List;
 
 public interface ProductService {
-    List<Product> GetAll();
     DataResult<List<Product>> getAll();
+    DataResult<List<Product>> getAllSorted();
+    DataResult<List<Product>> getAll(int pageNo,int pageSize);
     Result add(Product product);
+    DataResult<Product> getByProductName(String productName);
+    DataResult<Product> getByProductNameAndCategory_CategoryId(String productName, int categoryId);
+    DataResult<List<Product>> getByProductNameOrCategory_CategoryId(String productName, int categoryId);
+    DataResult<List<Product>> getByCategoryIdIn(List<Integer> categories);
+    DataResult<List<Product>> getByProductNameContains(String productName);
+    DataResult<List<Product>> getByProductNameStartsWith(String productName);
+    DataResult<List<Product>> getByNameAndCategory_CategoryId(String productName, int categoryId);
 }
